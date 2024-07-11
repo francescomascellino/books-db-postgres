1b533a88b2aac701a7d29e7ca39b625b1761332f
francesco.mascellino <foggolo@hotmail.com>
2024-07-11 14:22:33 +0200

    REmove unused imports

--- src/resources/postbook/postbook.module.ts
+++ src/resources/postbook/postbook.module.ts
@@ -3,19 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostbookService } from './postbook.service';
 import { PostbookController } from './postbook.controller';
 import { Postbook } from './entities/postbook.entity';
-import { Postuser } from '../postuser/entities/postuser.entity';
-import { PostuserModule } from '../postuser/postuser.module';
 import { PostuserPostbook } from '../postuser_postbook/entities/postuser_postbook.entity';
 import { PostuserPostbookModule } from '../postuser_postbook/postuser_postbook.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Postbook, Postuser, PostuserPostbook]),
-    forwardRef(() => PostuserModule),
+    TypeOrmModule.forFeature([Postbook, PostuserPostbook]),
     forwardRef(() => PostuserPostbookModule),
   ],
   controllers: [PostbookController],
   providers: [PostbookService],
-  exports: [TypeOrmModule.forFeature([Postbook, Postuser])],
+  exports: [TypeOrmModule.forFeature([Postbook])],
 })
 export class PostbookModule {}
--- src/resources/postuser/postuser.module.ts
+++ src/resources/postuser/postuser.module.ts
@@ -9,13 +9,13 @@ import { Postbook } from '../postbook/entities/postbook.entity';
 import { PostuserPostbookModule } from '../postuser_postbook/postuser_postbook.module';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Postbook, Postuser, PostuserPostbook]),
+    TypeOrmModule.forFeature([Postbook, PostuserPostbook]),
     forwardRef(() => PostbookModule),
     forwardRef(() => PostuserPostbookModule),
   ],
 
   controllers: [PostuserController],
   providers: [PostuserService],
-  exports: [TypeOrmModule.forFeature([Postuser]), PostuserService],
+  exports: [TypeOrmModule.forFeature([Postuser])],
 })
 export class PostuserModule {}
